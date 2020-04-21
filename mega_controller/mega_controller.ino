38319e6bc682c95261aa1a5c5ea46fcac1f1ce27
Tanjera <ibi.keller@gmail.com>
2020-04-20 21:38:37 -0400

    button matrix adjusted for prototype build

--- mega_controller/mega_controller.ino
+++ mega_controller/mega_controller.ino
@@ -18,13 +18,16 @@ https://github.com/tttapa/MIDI_controller
 #include "MIDI_Controller.h" // Include the library
 
 const uint8_t velocity = 0b1111111; // Maximum velocity (0b1111111 = 0x7F = 127)
-const uint8_t addresses[1][2] = {   // the note numbers corresponding to the buttons in the matrix
-  {  1, 2  },
+const uint8_t addresses[4][5] = {   // the note numbers corresponding to the buttons in the matrix
+  {  1, 2, 3, 4, 5  },
+  {  6, 7, 8, 9, 10  },
+  {  11, 12, 13, 14, 15  },
+  {  16, 17, 18, 19, 20  }
 };
 
 // Create a new instance of the class 'ButtonMatrix', called 'buttonmatrix', with dimensions 4 rows and 3 columns, with the rows connected to pins 2, 3, 4 and 5
 // and the columns connected to pins 6, 7 and 8, that sends MIDI messages with the notes specified in 'addresses' on MIDI channel 1, with velocity 127
-ButtonMatrix<1, 2> buttonmatrix({2}, {7, 8}, addresses, 1, velocity);
+ButtonMatrix<4, 5> buttonmatrix({2, 3, 4, 5}, {8, 9, 10, 11, 12}, addresses, 1, velocity);
 
 USBDebugMIDI_Interface midiInterface(115200);
 HairlessMIDI_Interface hairlessInterface;
